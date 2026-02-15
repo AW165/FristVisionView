66e8c2bf81564a8811fe77467f37b802b4b76b9d
Cxixi <c3136459784@163.com>
2026-02-15 13:23:49 +0800

    下拉列表已搞定

--- FristVisionView/SubPage/SettingPage.xaml.cs
+++ FristVisionView/SubPage/SettingPage.xaml.cs
@@ -12,6 +12,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using FirstVisionView.SubPage.CameraParament;
 
 namespace FirstVisionView
 {
@@ -20,9 +21,64 @@ namespace FirstVisionView
     /// </summary>
     public partial class SettingPage : UserControl
     {
+        private CameraSetting _CameraNumPage1;
+        private CameraSetting _CameraNumPage2;
+        private CameraSetting _CameraNumPage3;
+        private CameraSetting _CameraNumPage4;
         public SettingPage()
         {
             InitializeComponent();
+            _CameraNumPage1 = new CameraSetting();
+            CameraSetingPage.Content = _CameraNumPage1;
+            CameraNumber.Items.Add(1);
+            CameraNumber.Items.Add(2);
+            CameraNumber.Items.Add(3);
+            CameraNumber.Items.Add(4);
+        }
+
+        private void CameraSet_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch ((int)CameraNumber.SelectedItem)
+            {
+                case 1:
+                    {
+                        if (_CameraNumPage1 != null)
+                        {
+                            _CameraNumPage1 = new CameraSetting();
+                        }
+                            CameraSetingPage.Content = _CameraNumPage1;
+                        break; 
+                    } 
+                case 2:
+                    {
+                        if (_CameraNumPage2 != null)
+                        {
+                            _CameraNumPage2 = new CameraSetting();
+                        }
+                        CameraSetingPage.Content = _CameraNumPage2;
+                        break;
+                    }
+            
+                case 3:
+                    {
+                        if (_CameraNumPage3 != null)
+                        {
+                            _CameraNumPage3 = new CameraSetting();
+                        }
+                        CameraSetingPage.Content = _CameraNumPage3;
+                        break;
+                    }
+                case 4:
+                    {
+                        if (_CameraNumPage4 != null)
+                        {
+                            _CameraNumPage4 = new CameraSetting();
+                        }
+                        CameraSetingPage.Content = _CameraNumPage4;
+                        break;
+                    }
+            }
+
         }
     }
 }
